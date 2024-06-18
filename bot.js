decef01c5f34ead0e4a2b0741e91250001b41d1e
xotiiic <143630859+xotiiic@users.noreply.github.com>
2024-06-18 22:19:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ app.get('/', (req, res) => {
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`Xotiic`);
 });
 
 printWatermark();
